3caef19ce0f94a0feb4a1a96b52306dfd5d3824f
Siddharth Shah <Sid@localhost.localdomain>
2017-06-20 13:32:13 +0530

    4

--- udata/src/umongo/Testclass.java
+++ udata/src/umongo/Testclass.java
@@ -23,26 +23,53 @@ public class Testclass {
           shards.add("PC1node5");
           shards.add("PC1node6");
           
- 		  final long startcon = System.nanoTime();
+ 		  
+          TreeSet<String> fcon1=new TreeSet<String>();
+          TreeSet<String> fcon2=new TreeSet<String>();
+          TreeSet<String> fcon3=new TreeSet<String>();
+          TreeSet<String> fcon4=new TreeSet<String>();
+          TreeSet<String> fcon5=new TreeSet<String>();
+          TreeSet<String> fcon6=new TreeSet<String>();
+         
+          TreeSet<String> fhrw1=new TreeSet<String>();
+          TreeSet<String> fhrw2=new TreeSet<String>();
+          TreeSet<String> fhrw3=new TreeSet<String>();
+          TreeSet<String> fhrw4=new TreeSet<String>();
+          TreeSet<String> fhrw5=new TreeSet<String>();
+          TreeSet<String> fhrw6=new TreeSet<String>();
+/////////////////////////////////////////////////////////////////////////          
+          final long startcon = System.nanoTime();
           ConsistentHash c1= new ConsistentHash(shards,10);
-        
+          
           
           long cc1=0,cc2=0,cc3=0,cc4=0,cc5=0,cc6=0,cc=0;
-          File flist = new File("/home/Sid/input50000");
+          File flist = new File("/home/Sid/input100000");
           for( File f : flist.listFiles()){
         	  String s=c1.getShard(f.getName());
-        	   if(s.equals("PC1node1"))
+        	   if(s.equals("PC1node1")){
         		   cc1++;
-        	   else if(s.equals("PC1node2"))
+        		   fcon1.add(f.getAbsolutePath());
+        	   }
+        	   else if(s.equals("PC1node2")){
             		   cc2++;
-        	   else if(s.equals("PC1node3"))
+        	   fcon2.add(f.getAbsolutePath());
+   	           }
+        	   else if(s.equals("PC1node3")){
             		   cc3++;
-        	   else if(s.equals("PC1node4"))
+        	   fcon3.add(f.getAbsolutePath());
+ 	  		   }
+        	   else if(s.equals("PC1node4")){
             		   cc4++;
-        	   else if(s.equals("PC1node5"))
+        	   fcon4.add(f.getAbsolutePath());
+               }
+        	   else if(s.equals("PC1node5")){
             		   cc5++;
-        	   else if(s.equals("PC1node6"))
+        	   fcon5.add(f.getAbsolutePath());
+          	   }
+        	   else if(s.equals("PC1node6")){
             		   cc6++;
+        	   fcon6.add(f.getAbsolutePath());
+         	   }
         	   
         	   cc++;
           }
@@ -66,24 +93,38 @@ public class Testclass {
    
           final long starthrw = System.nanoTime();
           HrwHash h1 = new HrwHash(shards);          
- 		  long hh1=0,hh2=0,hh3=0,hh4=0,hh5=0,hh6=0,hh=0;
+ 		  
+          
+          long hh1=0,hh2=0,hh3=0,hh4=0,hh5=0,hh6=0,hh=0;
 
           for( File f : flist.listFiles()){
         	  String s=c1.getShard(f.getName());
         	  s= h1.determine_responsible_node(f.getName());
                   	   
-        	  if(s.equals("PC1node1"))
-          		   hh1++;
-          	   else if(s.equals("PC1node2"))
+        	  if(s.equals("PC1node1")){
+          		       hh1++;
+          		       fhrw1.add(f.getAbsolutePath());
+        	  }
+          	  else if(s.equals("PC1node2")){
               		   hh2++;
-          	   else if(s.equals("PC1node3"))
+              		   fhrw2.add(f.getAbsolutePath());
+         	  }
+          	  else if(s.equals("PC1node3")){
               		   hh3++;
-          	   else if(s.equals("PC1node4"))
+              		   fhrw3.add(f.getAbsolutePath());
+         	  }
+          	  else if(s.equals("PC1node4")){
               		   hh4++;
-          	   else if(s.equals("PC1node5"))
+              		   fhrw4.add(f.getAbsolutePath());
+         	  }
+          	  else if(s.equals("PC1node5")){
               		   hh5++;
-          	   else if(s.equals("PC1node6"))
+              		   fhrw5.add(f.getAbsolutePath());
+         	  }
+          	  else if(s.equals("PC1node6")){
               		   hh6++;
+              		   fhrw6.add(f.getAbsolutePath());
+         	  }
           	   
           	   hh++;
              
@@ -104,24 +145,18 @@ public class Testclass {
           System.out.println(durhrw+"\n");
           System.out.println(durationhrw+" seconds\n"+"--------------------------------------------\n");
           
-          
-          
-          
-          
-          
-          
-          
-          
+////////////////////////////////////////////////////////////////////////////        
+        
           
           /*     
           MongoClient mongo11 = new MongoClient("localhost",27017);
          DB db11             = mongo11.getDB("Info");
-        //  MongoClient mongo12 = new MongoClient("localhost",27018);
-       //   DB db12             = mongo12.getDB("Info");
-        //  MongoClient mongo13 = new MongoClient("localhost",27019);
-         // DB db13             = mongo13.getDB("Info");
-       //   MongoClient mongo14 = new MongoClient("localhost",27022);
-      //    DB db14             = mongo14.getDB("Info");
+       //  MongoClient mongo12 = new MongoClient("localhost",27018);
+       //  DB db12             = mongo12.getDB("Info");
+      //   MongoClient mongo13 = new MongoClient("localhost",27019);
+      //   DB db13             = mongo13.getDB("Info");
+     //    MongoClient mongo14 = new MongoClient("localhost",27022);
+      //   DB db14             = mongo14.getDB("Info");
          
        */
      }
