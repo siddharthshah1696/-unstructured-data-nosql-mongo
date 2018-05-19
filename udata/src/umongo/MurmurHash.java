e99888f5f76407ac076347a6fdba44092b9f9b31
siddharthshah1696 <lordzeus16@gmail.com>
2018-05-19 17:35:50 +0530

    Update MurmurHash.java

--- udata/src/umongo/MurmurHash.java
+++ udata/src/umongo/MurmurHash.java
@@ -1,22 +1,6 @@
 package umongo;
 
-/**
- *  The MurmurHash3 algorithm was created by Austin Appleby and placed in the public domain.
- *  This java port was authored by Yonik Seeley and also placed into the public domain.
- *  The author hereby disclaims copyright to this source code.
- *  <p>
- *  This produces exactly the same hash values as the final C++
- *  version of MurmurHash3 and is thus suitable for producing the same hash values across
- *  platforms.
- *  <p>
- *  The 32 bit x86 version of this hash should be the fastest variant for relatively short keys like ids.
- *  murmurhash3_x64_128 is a good choice for longer strings or if you need more than 32 bits of hash.
- *  <p>
- *  Note - The x86 and x64 versions do _not_ produce the same results, as the
- *  algorithms are optimized for their respective platforms.
- *  <p>
- *  See http://github.com/yonik/java_util for future updates to this file.
- */
+
 public final class MurmurHash {
 
   /** 128 bits of state */
